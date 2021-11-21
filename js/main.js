0494bea68c563996848b83882dfba8cb961a61a6
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2021-11-21 23:51:46 +0400

    add casino home part

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 $(document).ready(function () {
 	// body...
 
+function getFirstPixelColor(img) {
+    const canvas = document.createElement('canvas');
+    canvas.width = 2;
+    canvas.height = 2;
+
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    return [...ctx.getImageData(1, 1, 1, 1).data];
+}
+
+async function getBackgroundColor(img){
+    return new Promise((resolve, reject) => {
+        if(!img.complete || img.naturalWidth === 0) {
+            img.addEventListener('load', () => resolve(img));
+        } else {
+            resolve(img);
+        }
+    }).then(getFirstPixelColor);
+}
+
+document.querySelectorAll('.js-back').forEach(async (target) => {
+    const img = target.querySelector('img');
+    const c = await getBackgroundColor(img);
+    c[3] /= 255;
+    target.style.backgroundColor = `rgba(${c.join(',')})`;
+});
 
 
 

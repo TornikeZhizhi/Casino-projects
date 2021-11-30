82de0fed7e4cd06c1e49e9298008b751b3761d3f
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2021-12-01 00:09:41 +0400

    add all page

--- js/main.js
+++ js/main.js
@@ -60,6 +60,31 @@ document.querySelectorAll('.js-back').forEach(async (target) => {
 
 
 
+	$(".contact_form input").focus(function(){
+
+		$(".label_img").removeClass("active")
+		$(this).next().next().addClass("active")
+	})
+
+	$(".contact_form input").blur(function(){
+		$(".label_img").removeClass("active")
+
+
+	})
+
+
+	$(".review_container input").focus(function(){
+
+		$(".label_img").removeClass("active")
+		$(this).next().next().addClass("active")
+	})
+	$(".review_container input").blur(function(){
+		$(".label_img").removeClass("active")
+
+
+	})
+
+
 
 	$('.casino_slider').slick({
 	  infinite: true,
@@ -69,4 +94,97 @@ document.querySelectorAll('.js-back').forEach(async (target) => {
 	  dots: true,
 });
 
-})
+
+
+// 	$('.home_slider').slick({
+// 	  infinite: true,
+// 	  slidesToShow: 3,
+// 	  slidesToScroll: 1,
+// 	  centerPadding: '100px',
+// 	  dots: false,
+// 	   arrows : false,
+
+// });
+
+
+// $('.home_slider')
+//   .on('init', () => {
+//     $('.slick-slide[data-slick-index="-2"]').addClass('lt2');
+//     $('.slick-slide[data-slick-index="-1"]').addClass('lt1');
+//     $('.slick-slide[data-slick-index="1"]').addClass('gt1');
+//     $('.slick-slide[data-slick-index="2"]').addClass('gt2');
+// })
+//   .slick({
+//   centerMode: true,
+//   centerPadding: 0,
+//   slidesToShow: 3,
+//    dots: false,
+// 	 arrows : false,
+// 	  autoplay: true,
+//   autoplaySpeed: 2000,
+// }).on('beforeChange', (event, slick, current, next) => {
+//   $('.slick-slide.gt2').removeClass('gt2');
+//   $('.slick-slide.gt1').removeClass('gt1');
+//   $('.slick-slide.lt1').removeClass('lt1');
+//   $('.slick-slide.lt2').removeClass('lt2');
+
+//   const lt2 = (current < next && current > 0) ? current - 1 : next - 2;
+//   const lt1 = (current < next && current > 0) ? current : next - 1;
+//   const gt1 = (current < next || next === 0) ? next + 1 : current;
+//   const gt2 = (current < next || next === 0) ? next + 2 : current + 1;
+
+//   $(`.slick-slide[data-slick-index="${lt2}"]`).addClass('lt2');
+//   $(`.slick-slide[data-slick-index="${lt1}"]`).addClass('lt1');
+//   $(`.slick-slide[data-slick-index="${gt1}"]`).addClass('gt1');
+//   $(`.slick-slide[data-slick-index="${gt2}"]`).addClass('gt2');
+
+//   // Clone processing when moving from 5 to 0
+//   if (current === 5 && next === 0) {
+//     $(`.slick-slide[data-slick-index="${current - 1}"]`).addClass('lt2');
+//     $(`.slick-slide[data-slick-index="${current}"]`).addClass('lt1');
+//     $(`.slick-slide[data-slick-index="${current + 2}"]`).addClass('gt1');
+//     $(`.slick-slide[data-slick-index="${current + 3}"]`).addClass('gt2');
+//   }
+
+//   // Clone processing when moving from 0 to 5
+//   if (current === 0 && next === 5) {
+//     $(`.slick-slide[data-slick-index="${current - 1}"]`).addClass('gt2');
+//     $(`.slick-slide[data-slick-index="${current}"]`).addClass('gt1');
+//     $(`.slick-slide[data-slick-index="${current - 2}"]`).addClass('lt1');
+//     $(`.slick-slide[data-slick-index="${current - 3}"]`).addClass('lt2');
+//   }
+
+
+
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 20,
+  speed: 800,
+
+  autoplay: {
+          delay: 2000,
+          disableOnInteraction: false,
+        },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+swiper.on('transitionStart', function() {
+  $(".swiper_progress").removeClass("test");
+  $(".swiper_progress").addClass("test");
+});
+
+
+});

b0fb83f189304b93ff762c21ddad89d6faed9e00
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2021-12-05 03:10:41 +0400

    add responsive

--- js/main.js
+++ js/main.js
@@ -116,65 +116,6 @@ document.querySelectorAll('.js-back').forEach(async (target) => {
 
 
 
-// 	$('.home_slider').slick({
-// 	  infinite: true,
-// 	  slidesToShow: 3,
-// 	  slidesToScroll: 1,
-// 	  centerPadding: '100px',
-// 	  dots: false,
-// 	   arrows : false,
-
-// });
-
-
-// $('.home_slider')
-//   .on('init', () => {
-//     $('.slick-slide[data-slick-index="-2"]').addClass('lt2');
-//     $('.slick-slide[data-slick-index="-1"]').addClass('lt1');
-//     $('.slick-slide[data-slick-index="1"]').addClass('gt1');
-//     $('.slick-slide[data-slick-index="2"]').addClass('gt2');
-// })
-//   .slick({
-//   centerMode: true,
-//   centerPadding: 0,
-//   slidesToShow: 3,
-//    dots: false,
-// 	 arrows : false,
-// 	  autoplay: true,
-//   autoplaySpeed: 2000,
-// }).on('beforeChange', (event, slick, current, next) => {
-//   $('.slick-slide.gt2').removeClass('gt2');
-//   $('.slick-slide.gt1').removeClass('gt1');
-//   $('.slick-slide.lt1').removeClass('lt1');
-//   $('.slick-slide.lt2').removeClass('lt2');
-
-//   const lt2 = (current < next && current > 0) ? current - 1 : next - 2;
-//   const lt1 = (current < next && current > 0) ? current : next - 1;
-//   const gt1 = (current < next || next === 0) ? next + 1 : current;
-//   const gt2 = (current < next || next === 0) ? next + 2 : current + 1;
-
-//   $(`.slick-slide[data-slick-index="${lt2}"]`).addClass('lt2');
-//   $(`.slick-slide[data-slick-index="${lt1}"]`).addClass('lt1');
-//   $(`.slick-slide[data-slick-index="${gt1}"]`).addClass('gt1');
-//   $(`.slick-slide[data-slick-index="${gt2}"]`).addClass('gt2');
-
-//   // Clone processing when moving from 5 to 0
-//   if (current === 5 && next === 0) {
-//     $(`.slick-slide[data-slick-index="${current - 1}"]`).addClass('lt2');
-//     $(`.slick-slide[data-slick-index="${current}"]`).addClass('lt1');
-//     $(`.slick-slide[data-slick-index="${current + 2}"]`).addClass('gt1');
-//     $(`.slick-slide[data-slick-index="${current + 3}"]`).addClass('gt2');
-//   }
-
-//   // Clone processing when moving from 0 to 5
-//   if (current === 0 && next === 5) {
-//     $(`.slick-slide[data-slick-index="${current - 1}"]`).addClass('gt2');
-//     $(`.slick-slide[data-slick-index="${current}"]`).addClass('gt1');
-//     $(`.slick-slide[data-slick-index="${current - 2}"]`).addClass('lt1');
-//     $(`.slick-slide[data-slick-index="${current - 3}"]`).addClass('lt2');
-//   }
-
-
 
 var swiper = new Swiper('.swiper', {
   slidesPerView: 3,
@@ -224,7 +165,8 @@ swiper.on('activeIndexChange', function() {
 
 $(".nav-container").click(function(){
 
-	$(".header .header_right").slideToggle()
+	$(".header .header_right").slideToggle();
+  $('body').toggleClass("overflow")
 })
 
 
@@ -245,5 +187,7 @@ $(".payment_header .toggler").click(function() {
   $(this).parent().toggleClass('toggle')
 
 })
+
+
 
 });

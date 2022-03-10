3e2f74c0095fd220cd43aa8dd0085c88bcb71b98
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2022-03-10 16:05:01 +0400

    s

--- js/main.js
+++ js/main.js
@@ -178,7 +178,38 @@ var swiper = new Swiper('.swiper', {
 
 
 swiper.on('activeIndexChange', function() {
+
   $(".swiper_progress").removeClass("test1");
+
+  
+function getFirstPixelColor(img) {
+    const canvas = document.createElement('canvas');
+    canvas.width = 2;
+    canvas.height = 2;
+
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    return [...ctx.getImageData(1, 1, 1, 1).data];
+}
+
+async function getBackgroundColor(img){
+    return new Promise((resolve, reject) => {
+        if(!img.complete || img.naturalWidth === 0) {
+            img.addEventListener('load', () => resolve(img));
+        } else {
+            resolve(img);
+        }
+    }).then(getFirstPixelColor);
+}
+
+document.querySelectorAll('.js-back').forEach(async (target) => {
+    const img = target.querySelector('img');
+    const c = await getBackgroundColor(img);
+    c[3] /= 255;
+    target.style.backgroundColor = `rgba(${c.join(',')})`;
+});
+
+
   setTimeout(() => {
     $(".swiper_progress").addClass("test1");
   }, 10)
